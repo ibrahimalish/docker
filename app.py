a84867974eb023df643882476bcd6d596358daa1
ibrahimaliiiiiiiiiiiiiiiiiiiiiiiii <ibrahimali10939@gmail.com>
2025-07-07 16:45:32 +0500

    docker:

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello my name syed is ali shah"
+    return "Hello my name syed ibrahim ali shah"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
